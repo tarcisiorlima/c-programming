f2b115d1b6955fc1b3dcc3553bc8b7504fc750be
Coursera Learner <nobody@nowhere.nul>
2021-04-20 04:08:18 +0000

    all filese

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,16 +5,14 @@
 
 
 void assert_card_valid(card_t c) {
-  int ans_value; // c card value
-  int ans_suit; // c card suit
   
-  //checking if the card value is valid
-  assert(ans_value >= 2);
-  assert(ans_value <= VALUE_ACE);
+    //checking if the card value is valid
+  assert(c.value >= 2);
+  assert(c.value <= VALUE_ACE);
 
   //checking if the card suit is valid
-  assert(ans_suit >= SPADES);
-  assert(ans_suit <= NUM_SUITS);
+  assert(c.suit >= SPADES);
+  assert(c.suit <= NUM_SUITS);
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
@@ -104,46 +102,64 @@ card_t card_from_letters(char value_let, char suit_let) {
     {
     case '2':
       temp.value = 2;
+      break;
     case '3':
       temp.value = 3;
+      break;
     case '4':
       temp.value = 4;
+      break;
     case '5':
       temp.value = 5;
+      break;
     case '6':
       temp.value = 6;
+      break;
     case '7':
       temp.value = 7;
+      break;
     case '8':
       temp.value = 8;
+      break;
     case '9':
       temp.value = 9;
+      break;
     case '0':
       temp.value = 10;
+      break;
     case 'J':
       temp.value = 11;
+      break;
     case 'Q':
       temp.value = 12;
+      break;
     case 'K':
       temp.value = 13;
+      break;
     case 'A':
       temp.value = 14;
-    default:
-      assert(0);
+      break;
+      //default:
+      //assert(0);
     }
   switch (suit_let)
     {
     case 's':
       temp.suit = SPADES;
+      break;
     case 'h':
       temp.suit = HEARTS;
+      break;
     case 'd':
       temp.suit = DIAMONDS;
+      break;
     case 'c':
       temp.suit = CLUBS;
-    default:
-      assert(0);
-    }	
+      break;
+      //default:
+      //assert(0);
+    }
+  assert_card_valid(temp);
   return temp;
 }
 
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,22 @@
 #include "cards.h"
-
+#include <stdio.h>
 int main(void) {
+  card_t testcard;
+  testcard = card_from_letters('K','d');
+  printf("teste func card_from_letter Kd : card value %d, card suit %d\n",testcard.value,testcard.suit);
 
+  testcard = card_from_num(51);
+  printf("test func card_from_num 51 : card value %d, card suit %d\n", testcard.value, testcard.suit);
+
+  testcard = card_from_num(50);
+  printf("test func card_from_num 50 : card value %d, card suit %d\n", testcard.value, testcard.suit);
+
+  testcard = card_from_num(49);
+  printf("test func card_from_num 49 : card value %d, card suit %d\n", testcard.value, testcard.suit);
+
+  testcard = card_from_num(48);
+  printf("test func card_from_num 48 : card value %d, card suit %d\n", testcard.value, testcard.suit);
+
+  testcard = card_from_num(47);
+  printf("test func card_from_num 47 : card value %d, card suit %d\n", testcard.value, testcard.suit);
 }
